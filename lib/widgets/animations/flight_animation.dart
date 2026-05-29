9ca29aba3624f305441b8ab54467a3cc82ffd2ea
fuadreza <fuadreza1011@gmail.com>
2026-05-29 11:38:38 +0700

    add flight animation

--- lib/widgets/animations/flight_animation.dart
+++ lib/widgets/animations/flight_animation.dart
@@ -1,6 +1,7 @@
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class FlightAnimation extends StatefulWidget {
   const FlightAnimation({super.key, this.duration});
@@ -18,7 +19,10 @@ class _FlightAnimationState extends State<FlightAnimation> with SingleTickerProv
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: widget.duration ?? const Duration(seconds: 2));
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration ?? const Duration(seconds: 2),
+    );
 
     _animation = CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn);
 
@@ -33,39 +37,58 @@ class _FlightAnimationState extends State<FlightAnimation> with SingleTickerProv
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final maxWidth = constraints.maxWidth;
-        final iconSize = 36.0;
-        final flightPathWidth = maxWidth - iconSize;
+    return Column(
+      children: [
+        VisibilityDetector(
+          key: UniqueKey(),
+          child: SizedBox(),
+          onVisibilityChanged: (info) {
+            if (info.visibleFraction > 0.0) {
+              _controller.forward();
+            } else {
+              _controller.stop();
+            }
+          },
+        ),
+        LayoutBuilder(
+          builder: (context, constraints) {
+            final maxWidth = constraints.maxWidth;
+            final iconSize = 36.0;
+            final flightPathWidth = maxWidth - iconSize;
 
-        return AnimatedBuilder(
-          animation: _animation,
-          builder: (context, child) {
-            final currentPosition = flightPathWidth * _animation.value;
+            return AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                final currentPosition = flightPathWidth * _animation.value;
 
-            return Stack(
-              alignment: Alignment.centerLeft,
-              children: [
-                Container(width: 0 + currentPosition, height: 2, color: Colors.grey),
-                Positioned(
-                  left: currentPosition,
-                  top: 0,
-                  bottom: 0,
-                  child: SizedBox(
-                    height: iconSize,
-                    width: iconSize,
-                    child: Transform.rotate(
-                      angle: -math.pi / -2,
-                      child: Icon(Icons.local_airport, size: iconSize, color: Colors.white),
-                    ),
+                return SizedBox(
+                  width: maxWidth,
+                  height: iconSize,
+                  child: Stack(
+                    alignment: Alignment.centerLeft,
+                    children: [
+                      Container(width: 0 + currentPosition, height: 2, color: Colors.grey),
+                      Positioned(
+                        left: currentPosition,
+                        top: 0,
+                        bottom: 0,
+                        child: SizedBox(
+                          height: iconSize,
+                          width: iconSize,
+                          child: Transform.rotate(
+                            angle: -math.pi / -2,
+                            child: Icon(Icons.local_airport, size: iconSize, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                );
+              },
             );
           },
-        );
-      },
+        ),
+      ],
     );
   }
 }
--- lib/widgets/contents/flight_content.dart
+++ lib/widgets/contents/flight_content.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import '../animations/flight_animation.dart';
@@ -10,25 +12,121 @@ class FlightContent extends StatefulWidget {
 }
 
 class _FlightContentState extends State<FlightContent> with SingleTickerProviderStateMixin {
-  late AnimationController _controllerText;
+  final ValueNotifier<int> minutes1 = ValueNotifier(0);
+  final ValueNotifier<int> minutes2 = ValueNotifier(0);
+  late Timer timer1;
+  late Timer timer2;
 
   @override
   void initState() {
     super.initState();
-    _controllerText = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    timer1 = Timer.periodic(Duration(milliseconds: 8), (timer) {
+      minutes1.value = (minutes1.value + 1);
+      if (minutes1.value >= 204) {
+        timer1.cancel();
+      }
+    });
+    timer2 = Timer.periodic(Duration(milliseconds: 8), (timer) {
+      minutes2.value = (minutes1.value + 1);
+      if (minutes1.value >= 142) {
+        timer2.cancel();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timer1.cancel();
+    timer2.cancel();
+    minutes1.dispose();
+    minutes2.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        FlightAnimation(
-          duration: Duration(seconds: 3),
-        ),
-        FlightAnimation(
-          duration: Duration(seconds: 2),
-        ),
-      ],
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'How long does it really take?',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 21,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Text(
+            'Sydney to Melbourne - curb to curb, your time preserved',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            height: 200,
+          ),
+          Text(
+            'Airlines',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 21,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          FlightAnimation(
+            duration: Duration(seconds: 3),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ValueListenableBuilder(
+              valueListenable: minutes1,
+              builder: (context, value, child) {
+                return Text(
+                  '$value minutes',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'BLACK JET',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 21,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          FlightAnimation(
+            duration: Duration(seconds: 2),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ValueListenableBuilder(
+              valueListenable: minutes2,
+              builder: (context, value, child) {
+                return Text(
+                  '$value minutes',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
